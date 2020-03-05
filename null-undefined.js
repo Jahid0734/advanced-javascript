b9e2ccc695b143f89f606c4b5d4f1096184fbcf5
unknown <Jahid0734@gmail.com>
2020-03-05 14:26:13 +0600

    null vs undefiend

---
+++ null-undefined.js
@@ -0,0 +1,17 @@
+// let pakhi;
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return 
+// }
+
+// function add(num1, num2){
+//     console.log(num1, num2)
+// }
+
+// const nasir = { name:"nasir", phone:"0174245"};
+// console.log(nasir.gf);
+let fun = undefined;
+console.log(fun);
+ 
+let ages = [2, 3, 5];
+console.log(ages[14]);
