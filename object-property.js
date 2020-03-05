3c7f11aea68b200dc5de1166b04a58dadc825764
unknown <Jahid0734@gmail.com>
2020-03-05 19:59:33 +0600

    arguments

--- object-property.js
+++ object-property.js
@@ -8,5 +8,5 @@ const students = [
 const names = students.map( s => s.name);
 const ids = students.map(s => s.id);
 const bigger = students.filter(s => s.id>30);
-const findHim = students.find(s => s.id>35);
+const findHim = students.find(s => s.id>30);
 console.log(findHim);
